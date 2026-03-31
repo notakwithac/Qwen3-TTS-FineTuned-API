eb66c711489d812ec3960dcf2b9566228336d676
Utcarsh Srivastava <utcarsh.srivastava1611@gmail.com>
2026-03-31 17:59:31 +0530

    feat: bug in finetuning - taken from parent

--- finetuning/sft_12hz.py
+++ finetuning/sft_12hz.py
@@ -624,7 +624,7 @@ def train_programmatic(
                     output_hidden_states=True
                 )
                 hidden_states = outputs.hidden_states[0][-1]
-                talker_hidden_states = hidden_states[codec_mask[:, 1:]]
+                talker_hidden_states = hidden_states[codec_mask[:, :-1]]
                 talker_codec_ids = codec_ids[codec_mask]
                 sub_talker_logits, sub_talker_loss = model.talker.forward_sub_talker_finetune(talker_codec_ids, talker_hidden_states)
                 loss = outputs.loss + sub_talker_loss
